a13981ad8079511ba990781991d97d16ca0c45de
root <root@DESKTOP-1NG1TEU.localdomain>
2021-09-05 21:16:41 +0700

    First Commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Navbar.css';
+
+class Navbar extends React.Component {
+    render() {
+        return(
+            <div>
+                <ul>
+                    <li >Home</li>
+                    <li >Member</li>
+                </ul>
+            </div>
+        )
+    }
+}
